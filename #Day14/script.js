298d72b2754268b68ab455fd66286b5076e49b81
kavyabagde <kavyabagde0606@gmail.com>
2024-09-04 20:21:30 +0530

    java script lecture 1 is completed

---
+++ #Day14/script.js
@@ -0,0 +1,36 @@
+// Qusetion no.  1
+let age = 23;
+age = age + 2;
+console.log("age after 2 years will : " + age);
+
+// Question no. 2
+let hindi = 80;
+let eng = 90;
+let mat = 100;
+let avg = (hindi + eng + mat) / 3;
+console.log("avg scoer is : " + avg);
+
+// Question no. 3
+let num = 5;
+let newNum = num++;
+console.log("newNum is = " + newNum);
+newNum = ++num;
+console.log("newNum 2 is = " + newNum);
+
+// Question no. 4
+let myName = "kavya bagde";
+console.log("length of my name is = " + (myName.length - 1));
+
+// Question no. 5
+let firstName = "kavya";
+console.log("first letter of name is = " + firstName[0]);
+
+// Question no. 6
+console.log("first letter of name is = " + firstName[firstName.length - 1]);
+
+// Question no. 7
+let emptyStr = "";
+let spaceStr = " ";
+
+console.log(emptyStr.length);
+console.log(spaceStr.length);
